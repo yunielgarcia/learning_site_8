15e7803819cd1ba42dd2a81ef9e1540716e285da
Yuniel Garcia <garciayuni84@gmail.com>
2018-06-06 18:17:12 -0400

    dummy commit

--- courses/models.py
+++ courses/models.py
@@ -9,7 +9,8 @@ class Course(models.Model):
     description = models.TextField()
     teacher = models.ForeignKey(User)
     subject = models.CharField(default='', max_length=100)
-    
+    published = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
 
@@ -19,51 +20,53 @@ class Step(models.Model):
     description = models.TextField()
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
-    
+
     class Meta:
         abstract = True
-        ordering = ['order',]
-    
+        ordering = ['order', ]
+
     def __str__(self):
         return self.title
 
 
 class Text(Step):
     content = models.TextField(blank=True, default='')
-    
+
     def get_absolute_url(self):
         return reverse('courses:text', kwargs={
-                'course_pk': self.course_id,
-                'step_pk': self.id
-            })
+            'course_pk': self.course_id,
+            'step_pk': self.id
+        })
 
 
 class Quiz(Step):
     total_questions = models.IntegerField(default=4)
-    
+    times_taken = models.IntegerField(default=0, editable=False)
+
     class Meta:
         verbose_name_plural = "Quizzes"
 
     def get_absolute_url(self):
         return reverse('courses:quiz', kwargs={
-                'course_pk': self.course_id,
-                'step_pk': self.id
-            })
+            'course_pk': self.course_id,
+            'step_pk': self.id
+        })
 
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz)
     order = models.IntegerField(default=0)
     prompt = models.TextField()
-    
+
     class Meta:
-        ordering = ['order',]
-        
+        ordering = ['order', ]
+
     def get_absolute_url(self):
         return self.quiz.get_absolute_url()
-    
+
     def __str__(self):
         return self.prompt
+
 
 class MultipleChoiceQuestion(Question):
     shuffle_answers = models.BooleanField(default=False)
@@ -78,9 +81,9 @@ class Answer(models.Model):
     order = models.IntegerField(default=0)
     text = models.CharField(max_length=255)
     correct = models.BooleanField(default=False)
-    
+
     class Meta:
-        ordering = ['order',]
-        
+        ordering = ['order', ]
+
     def __str__(self):
         return self.text
