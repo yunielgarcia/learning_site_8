b4e9d974c35392806050902388ff03964fe2397f
Yuniel Garcia <garciayuni84@gmail.com>
2018-06-15 09:25:01 -0400

    dummy commit 1

--- courses/admin.py
+++ courses/admin.py
@@ -1,11 +1,63 @@
 from django.contrib import admin
+from datetime import date
 
 from . import models
 
 
-admin.site.register(models.Course)
+class TextInline(admin.StackedInline):
+    model = models.Text
+
+
+class QuizInline(admin.StackedInline):
+    model = models.Quiz
+
+
+class AnswerInline(admin.StackedInline):
+    model = models.Answer
+
+
+class YearListFilter(admin.SimpleListFilter):
+    title = 'year created'
+    parameter_name = 'year'
+
+    def lookups(self, request, model_admin):
+        """Return what is seeing in the side bar. The first parameter is for the url
+        and the second for the link to click"""
+        return (
+            ('2015', '2015'),
+            ('2016', '2016')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '2015':
+            return queryset.filter(created_at__gte=date(2015, 1, 1),
+                                   created_at__lte=date(2015, 12, 31))
+        if self.value() == '2016':
+            return queryset.filter(created_at__gte=date(2016, 1, 1),
+                                   created_at__lte=date(2016, 12, 31))
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInline, QuizInline]
+    search_fields = ['title', 'description']
+    list_filter = ['created_at', YearListFilter]
+    list_display = ['title', 'created_at', 'published', 'time_to_complete']
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline, ]
+    search_fields = ['prompt']
+    list_display = ['prompt', 'quiz', 'order']
+    list_editable = ['quiz', 'order']
+
+
+admin.site.register(models.Course, CourseAdmin)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz)
-admin.site.register(models.MultipleChoiceQuestion)
-admin.site.register(models.TrueFalseQuestion)
+admin.site.register(models.Quiz, QuizAdmin)
+admin.site.register(models.MultipleChoiceQuestion, QuestionAdmin)
+admin.site.register(models.TrueFalseQuestion, QuestionAdmin)
 admin.site.register(models.Answer)
--- courses/models.py
+++ courses/models.py
@@ -14,6 +14,12 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+    def time_to_complete(self):
+        # We do this here to avoid recursive importing since course_extras
+        # also import models.Course...
+        from courses.templatetags.course_extras import time_estimate
+        return '{} min.'.format(time_estimate(len(self.description.split())))
+
 
 class Step(models.Model):
     title = models.CharField(max_length=255)
